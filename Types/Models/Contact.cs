1a89ea92faeeef4ad4e860d0d874f1145c62a7cd
Eric Smith <ydrive65@yahoo.com>
2023-06-24 19:38:33 -0400

    Quickstart

---
+++ Types/Models/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace Types.Models;
+
+public class Contact
+{
+    public required string Name { get; set; }
+    public required int PhoneNumber { get; set; }
+    public required string Email { get; set; }
+}
